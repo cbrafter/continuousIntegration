7c87b66878d7e1e137d7f08cb7b1135c51927105
Craig Benjamin Rafter <craigrafter@Craigs-MacBook-Air.local>
2016-02-22 17:09:09 +0000

    added value tests to test_fibonacci

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,9 +1,16 @@
-def test_fibonacci():
-	"""
-	In this unciton we import fibonnacci.py
-	and test we get the correct sequence
-	"""
-	import fibonacci as fb
-	assert fb.Fibonacci(10) == 55
-	assert fb.Fibonacci(5) == 5
+import unittest as ut
 
+class FibTest(ut.TestCase):
+	
+	def test_fibonacci(self):
+		"""
+		In this unciton we import fibonnacci.py
+		and test we get the correct sequence
+		"""
+		import fibonacci as fb
+		assert fb.Fibonacci(10) == 55
+		assert fb.Fibonacci(5) == 5
+
+	def test_Fib_throws_exeption(self):
+		self.assertRaises(ValueError, fb.Fibonacci, -5)
+		self.assertRaises(ValueError, fb.Fibonacci, 5.5)
