101d70367284945399db214cf0aea6367ac0a611
Craig Benjamin Rafter <craigrafter@Craigs-MacBook-Air.local>
2016-02-22 17:05:01 +0000

    added input checking to fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,11 @@
 def Fibonacci(n):
-	a,b = 1,1
-	for i in range(n-1):
-		a,b = b,a+b
-	return a
+	if n < 0:
+		raise ValueError('n<0 invalid')
+	elif type(n) != int:
+		raise ValueError('n not int')
+	else:
+		a,b = 1,1
+		for i in range(n-1):
+			a,b = b,a+b
+		return a
 
